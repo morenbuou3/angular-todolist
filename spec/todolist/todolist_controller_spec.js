3011aa505e2414408430ba0a80372ba4627ac755
jhwang <332409662@qq.com>
2018-11-15 11:12:13 +0800

    add test for todolistController.

--- spec/todolist/todolist_controller_spec.js
+++ spec/todolist/todolist_controller_spec.js
@@ -15,5 +15,12 @@ describe("Midway: Testing Controller", function() {
       var controller = $controller('todolistController', { $scope: $scope });
       expect($scope.todos.length).toEqual(2);
     });
+
+    it('test the todos default has Learn AngularJS item', function() {
+      var $scope = $rootScope.$new();
+      var controller = $controller('todolistController', { $scope: $scope });
+      expect($scope.todos[0].text).toEqual('Learn AngularJS');
+      expect($scope.todos[0].done).not.toBeTruthy();
+    });
   });
 });
